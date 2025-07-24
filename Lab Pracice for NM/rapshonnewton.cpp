5db720acc1af63dd8c180f85f65197fad38269d3
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-07-24 23:55:40 +0600

    updated newtonraphson

--- Lab Pracice for NM/rapshonnewton.cpp
+++ Lab Pracice for NM/rapshonnewton.cpp
@@ -1,41 +1,27 @@
-// C++ program for implementation of Newton Raphson Method for
-// solving equations
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define EPSILON 0.001
 using namespace std;
 
-// An example function whose solution is determined using
-// Bisection Method. The function is x^3 - x^2  + 2
-double func(double x)
-{
-    return x*x*x - x*x + 2;
+double func(double x) {
+    return x*x*x - x - 2;
 }
 
-// Derivative of the above function which is 3*x^x - 2*x
-double derivFunc(double x)
-{
-    return 3*x*x - 2*x;
+double derivFunc(double x) {
+    return 3*x*x - 1;
 }
 
-// Function to find the root
-void newtonRaphson(double x)
-{
+void newtonRaphson(double x) {
     double h = func(x) / derivFunc(x);
-    while (abs(h) >= EPSILON)
-    {
-        h = func(x)/derivFunc(x);
- 
-        // x(i+1) = x(i) - f(x) / f'(x)  
+    while (abs(h) >= EPSILON) {
+        h = func(x) / derivFunc(x);
+        cout << "x = " << x << ", f(x) = " << func(x) << ", h = " << h << endl;
         x = x - h;
     }
-
-    cout << "The value of the root is : " << x;
+    cout << "The value of the root is : " << x << endl;
 }
 
-// Driver program to test above
-int main()
-{
-    double x0 = -20; // Initial values assumed
+int main() {
+    double x0 = 1.5;
     newtonRaphson(x0);
     return 0;
 }
